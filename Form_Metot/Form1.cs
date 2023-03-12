415691a95b39137432264e93eaf7feb0a419040c
m.sinans <msomercikoglu@gmail.com>
2023-03-12 13:10:30 +0300

    Add project files.

---
+++ Form_Metot/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Form_Metot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Islem islem = new Islem();
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            int s1 = Convert.ToInt32(textBox1.Text);
+            int s2 = Convert.ToInt32(textBox2.Text);
+            int s3 = islem.Topla(s1, s2);
+            label2.Text = s3.ToString();            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(textBox1.Text);
+            int s2 = Convert.ToInt32(textBox2.Text);
+            int s3 = islem.Cikar(s1, s2);
+            label2.Text = s3.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double s1 = Convert.ToDouble(textBox1.Text);
+            double s2 = Convert.ToDouble(textBox2.Text);
+            double s3 = islem.Bol(s1, s2);
+            label2.Text = s3.ToString(("0.00"));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int s1 = Convert.ToInt32(textBox1.Text);
+            int s2 = Convert.ToInt32(textBox2.Text);
+            int s3 = islem.Carp(s1,s2);
+            label2.Text = s3.ToString();
+        }
+    }
+}
---
+++ Form_Metot/Islem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form_Metot
+{
+    internal class Islem
+    {
+        public int Topla(int s1, int s2)
+        {
+            int s3 = s1 + s2;
+            return s3;
+        }
+        public int Cikar(int s1, int s2)
+        {
+            int s3 = s1 - s2;
+            return s3;
+        }
+        public double Bol(double s1, double s2)
+        {
+            double s3 = s1 / s2;
+            return s3;
+        }
+        public int Carp(int s1, int s2)
+        {
+            int s3 = s1 * s2;
+            return s3;
+
+        }
+    }
+}
